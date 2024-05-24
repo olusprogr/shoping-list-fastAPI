0e851cb743b9566719a4d481dce32cdd0f59ccf0
olusprogr <olus.main333@gmail.com>
2024-05-24 15:30:18 +0200

    fixed bugs

--- main.py
+++ main.py
@@ -1,48 +1,73 @@
 from fastapi import FastAPI, Query, Path
 from typing import Annotated, Union, get_type_hints
-from models import ShopingListItem
+from models import ShoppingList
+import time
 from errorhandlers import *
 
-shopingLists: list[ShopingListItem] = [
-    # ShopingList(title="My Shoping List", date=time.strftime("%Y-%m-%d %H:%M:%S"), items={1: {"name": "Milk", "quantity": "1L"}}),
+shoppingLists: list[ShoppingList] = [
+    ShoppingList(title="1", date=time.strftime("%Y-%m-%d %H:%M:%S"), items=[{"name": "Milk", "quantity": "1L"}], img={"url": "test.png", "name": "image"}),
     # ShopingList(title="My Shoping List 2", date=time.strftime("%Y-%m-%d %H:%M:%S"), items={1: {"name": "Bread", "quantity": "1"}})
 ]
 
 
 app = FastAPI()
 
-@app.post("/shopinglist/create/newList")
+@app.post("/shoppinglist/create/newList")
 async def create_list(*,
-    shopingListItem: ShopingListItem, # example value from models.py
-    title: Annotated[str, Query(
-        min_length=3,
-        max_length=15,
-        title="Title of the shoping list"
-        )],
-    ) -> Union[ShopingListItem, dict[str, str]]:
-
-    for list in shopingLists:
+    shopingListItem: ShoppingList, # example value from models.py
+    title: Annotated[
+        str, Query(
+            min_length=3,
+            max_length=20,
+            title="Title of the shopping list"
+        )
+    ],
+    img: Annotated[
+        str, Query(
+            min_length=4,
+            title="Image data"
+        )
+    ] = None
+    ) -> Union[ShoppingList, dict[str, str]]:
+    for list in shoppingLists:
         if list.title == title:
             return {"error": "List with this title already exists"}
         
     shopingListItem.title = title
-    shopingLists.append(shopingListItem)
-    print(shopingLists)
+    shopingListItem.items = []
+    shoppingLists.append(shopingListItem)
+    print(shoppingLists)
     return shopingListItem
 
-@app.post("/shopinglist/addItems/{shopingListTitle}/newItems")
-async def add_item(
-    shopingListTitle: Annotated[str, Query(
-        min_length=3,
-        max_length=15,
-        title="Title of the shoping list"
-        )],
-    name: str,
-    quantity: Annotated[int, Path(title="Quantity of the item", ge=1, le=1000)]
-    ) -> dict[str, str]:
-    for list in shopingLists:
-        if list.title == shopingListTitle:
-            next_item: dict[str, str] = {"name": name, "quantity": quantity}
+@app.post("/shoppinglist/addItems/{shopingListTitle}/newItems")
+async def add_item(*,
+    shoppingListItem: ShoppingList,
+    shoppingListTitle: Annotated[
+        str,
+        Query(
+            min_length=3,
+            max_length=15,
+            title="Title of the shopping list"
+        )
+    ] = "1",
+    item: str,
+    price: int | None,
+    quantity: Annotated[
+        int,
+        Path(
+            title="Quantity of the item",
+            ge=1,
+            le=1000
+        )
+    ] = 1
+    ):
+
+    for list in shoppingLists:
+        if list.title == shoppingListTitle:
+
+            print(list.items)
+
+            """next_item: dict[str, str] = {"name": name, "quantity": quantity}
             next_index: int = max(list.items.keys()) + 1 if list.items else 1
             
             for item in list.items.values():
@@ -50,21 +75,21 @@ async def add_item(
                     return {"error": "Item with this name already exists"}
                 
             list.items[next_index] = next_item
-            return {"message": "Item added successfully"}
+            return {"message": "Item added successfully"}"""
 
     return {"error": "List with this title does not exist"}
 
-@app.get("/shopinglist/getList/{shopingListTitle}")
-async def get_list(shopingListTitle: str):
-    for list in shopingLists:
-        if list.title == shopingListTitle:
+@app.get("/shoppinglist/getList/{shopingListTitle}")
+async def get_list(shoppingListTitle: str):
+    for list in shoppingLists:
+        if list.title == shoppingListTitle:
             return list
     return {"error": "List with this title does not exist"}
 
-@app.delete("/shopinglist/deleteList/{shopingListTitle}")
-async def delete_list(shopingListTitle: str):
-    for list in shopingLists:
-        if list.title == shopingListTitle:
-            shopingLists.remove(list)
+@app.delete("/shoppinglist/deleteList/{shopingListTitle}")
+async def delete_list(shoppingListTitle: str):
+    for list in shoppingLists:
+        if list.title == shoppingListTitle:
+            shoppingLists.remove(list)
             return {"message": "List deleted successfully"}
     return {"error": "List with this title does not exist"}
--- models.py
+++ models.py
@@ -1,7 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import time
 
-class ShopingListItem(BaseModel):
-    title: str = "My Shoping Item"
-    date: str = time.strftime("%Y-%m-%d %H:%M:%S")
-    items: dict[int, dict[str, str]] = {}
+class Image(BaseModel):
+    url: str | None = Field(
+        default=None, title="URL of the image"
+    )
+    name: str = Field(
+        default="My Image", title="Name of my image"
+    )
+
+class ShoppingListItem(BaseModel):
+    amount: int = 1
+    item: str = ""
+    price: float | None = None
+
+class ShoppingList(BaseModel):
+    title: str = Field(
+        default="My Shopping List", title="Title of the shopping list", max_length=40
+    )
+    date: str = Field(
+        default=time.strftime("%Y-%m-%d %H:%M:%S"), title="Creation date of your shopping list"
+    )
+    items: list[ShoppingListItem] = Field(
+        default_factory=list, title="List of items in the shopping list"
+    )
+    img: Image
+
